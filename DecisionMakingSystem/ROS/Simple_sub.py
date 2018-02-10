33af485f53ad16602e85d47b9740ceb644ea8be7
utkarshanand <anand.utkarsh96@gmail.com>
2018-02-09 22:03:30 -0500

    Simple_sub
    Subscribing to throttle_report.

---
+++ DecisionMakingSystem/ROS/Simple_sub.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float32
+ 
+def callback(data):
+     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def listener():
+	rospy.init_node('listener', anonymous=True)
+
+	rospy.Subscriber("/truevision/throttle_report", Float32, callback)
+
+	rospy.spin()
+
+if __name__ == '__main__':
+	listener()
